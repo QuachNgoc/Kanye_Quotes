2fefb9bdea29e75361cd2a2c33a5346f7ea531af
Ngoc <80575560+QuachNgoc@users.noreply.github.com>
2021-08-01 19:11:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,24 +30,4 @@ kanye_button.grid(row=1, column=0)
 
 window.mainloop()
 '''
-MY_LAT = 0.00
-MY_LNG = 0.00
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LNG,
-    "formatted":0,
-}
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-
-print(sunrise)
-print(sunset)
-
-time_now = datetime.now()
-
-print(time_now.hour)
